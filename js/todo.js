b63d5308856669e2f12aed97acd1fc329f9e609c
vivianabepa <vivianabenavides@Vivs-MacBook-Pro.local>
2014-12-01 16:38:03 -0500

    Mejoras
    mejoras

--- js/todo.js
+++ js/todo.js
@@ -1,67 +1,73 @@
-var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 13,
-  center: new google.maps.LatLng(3.382621, -76.541985),
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-});
+var canvas = document.getElementById("graphCanvas");
+var context = canvas.getContext("2d");
 
-function dragStart(ev) {
-   ev.dataTransfer.effectAllowed='move';
-   ev.dataTransfer.setData("Text", ev.target.getAttribute('id'));
-   ev.dataTransfer.setDragImage(ev.target,0,0);
-   var id = ev.target.getAttribute('id');
 
-   $.ajax({
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    context.fillStyle="#FFFFFF";
+    context.fillRect(0,0,340,200); 
+    context.drawImage(document.getElementById(data), 150, 75);
+
+    if(data == "drag1"){
+        $("#iconoActual").val("restaurantes");
+    }
+    if(data == "drag2"){
+        $("#iconoActual").val("hoteles");
+    }
+    if(data == "drag3"){
+        $("#iconoActual").val("cajeros");
+    }
+
+    iconoActual = $("#iconoActual").val();  
+
+    console.log(iconoActual);
+
+    $.ajax({
       type: "GET",
       url: "php/places.php",
-      data: { PlacesID: id}
+      data: { iconoActual: iconoActual}
     }).done(function(){
       console.log("Solicitud enviada al API");
     }).success(function(result){
-      resultado = JSON.parse(result);
-      console.log(resultado);
+      var resultado = JSON.parse(result);
+
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: new google.maps.LatLng(3.382621, -76.541985),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+
       var infowindow = new google.maps.InfoWindow();
       var marker, i;
 
-      if (!resultado.error) {
-        for (i=0; i<resultado.locations.length; i++){
-          	marker = new google.maps.Marker({
-            position: new google.maps.LatLng(locations[i][1]),
-            map: map
-          });
+      
+      for (i = 0; i < resultado.length; i++) {
+        console.log(i);
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(resultado[i][2], resultado[i][3]),
+          map: map
+        });
 
-          google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-              infowindow.setContent(locations[i][0]);
-              infowindow.open(map, marker);
-            }
-          })(marker, i));
-        }
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+          
+          return function() {
+            infowindow.setContent(resultado[i][2]);
+            infowindow.open(map, marker);
+          }          
+
+        })(marker, i));
       }
     }).error(function(error){
       console.log("Error: "+error);
     })
-   return true;
 }
 
-function dragEnter(ev) {
-   event.preventDefault();
-   return true;
-}
-
-function dragOver(ev) {
-    return false;
-}
-
-function dragDrop(ev) {
-   var src = ev.dataTransfer.getData("Text");
-   ev.target.appendChild(document.getElementById(src));
-   
-   ev.stopPropagation();
-   return false;
-}
-
-function arrastrando(e){
-	elemento=e.target;
-	e.dataTransfer.setData('Text',elemento.getAttribute('id'));
-	e.dataTransfer.setDragImage(e.target,62,62);
-}
